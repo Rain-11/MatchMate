ea278667bda6c62e213263e91dd3d4db4fa749dc
CrazyRain <1147932729@qq.com>
2024-04-12 17:45:12 +0800

    更改后端服务端口

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://47.115.212.172:8080'
+    baseURL: 'https://47.115.212.172:8081'
 });
 /* {
   baseURL: 'http://localhost:8080',
